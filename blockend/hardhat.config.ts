26ae464ab3a4d91f59f928d7f2f992aaf2dad0e5
alex-alra-arteaga <alexarteagacontijoch@gmail.com>
2023-07-07 18:13:24 +0200

    [add] init project

---
+++ blockend/hardhat.config.ts
@@ -0,0 +1,69 @@
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+dotenv.config();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const config: HardhatUserConfig = {
+  solidity:  {
+    compilers: [
+      {
+        version: "0.8.9",
+      },
+      {
+        version: "0.5.16",
+      },
+      {
+        version: "0.6.6",
+      }
+    ],
+  },
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    xdc: {
+      chainId: 50,
+      url: "https://rpc.xinfin.network",
+      accounts: [PRIVATE_KEY],
+    },
+    apothem: {
+      chainId: 51,
+      url: "https://rpc.apothem.network",
+      accounts: [PRIVATE_KEY],
+    },
+    sepolia: {
+      chainId: 11155111,
+      url: "https://rpc.ankr.com/eth_sepolia",
+      accounts: [PRIVATE_KEY],
+    }
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
+
+export default config;
