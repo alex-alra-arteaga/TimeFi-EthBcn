d9cbc1724c62e6f06e16bb0fda8ccc6b4d4283ce
alex-alra-arteaga <alexarteagacontijoch@gmail.com>
2023-07-07 20:47:26 +0200

    [add] wXDC mock importation

---
+++ blockend/contracts/mocks/wXDC/IwXDC.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity =0.8.9;
+
+interface IWXDC {
+    function deposit() external payable;
+    function withdraw(uint wad) external;
+    function totalSupply() external view returns (uint);
+    function approve(address guy, uint wad) external returns (bool);
+    function transfer(address dst, uint wad) external returns (bool);
+    function transferFrom(address src, address dst, uint wad) external returns (bool);
+
+    event Approval(address indexed src, address indexed guy, uint wad);
+    event Transfer(address indexed src, address indexed dst, uint wad);
+    event Deposit(address indexed dst, uint wad);
+    event Withdrawal(address indexed src, uint wad);
+
+    function name() external view returns (string memory);
+    function symbol() external view returns (string memory);
+    function decimals() external view returns (uint8);
+    function balanceOf(address account) external view returns (uint);
+    function allowance(address owner, address spender) external view returns (uint);
+}
